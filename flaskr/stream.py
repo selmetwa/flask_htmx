0379d9099f263bc765ecbcb2a1a58844607d7d9e
Sherif Elmetwally <sge5@njit.edu>
2023-09-24 21:31:46 -0400

    done

--- flaskr/stream.py
+++ flaskr/stream.py
@@ -54,7 +54,77 @@ def stream_details(id):
   tasksInStream = []
   for row in sqliteRowsTasksInStream:
     tasksInStream.append(dict(row))
-  print(stream)
-  print(tasksInStream)
   
   return render_template('streams/stream_details.html', stream=stream, tasks=tasksInStream)
+
+
+@bp.route('/render_add_task_to_stream_view/<id>', methods=(['GET']))
+def render_add_task_to_stream_view(id):
+    return '''
+    <form class="form" hx-post="/add_task_to_stream/{}" hx-target=".task-list" hx-swap="innerHTML" hx-on::after-request="this.reset()">
+      <h2>Create New Task</h2>
+      <label>Task Title</label>
+      <input type="text" name="title" id="title" />
+      <label for="priority">Choose a priority level from 1-5</label>
+      <select name="priority" id="priority">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <label for="description">Description</label>
+      <textarea name="description" id="description">
+      </textarea>
+      <button type="submit">Submit</button>
+    </form>
+'''.format(id)
+
+@bp.route('/add_task_to_stream/<id>', methods=(['POST']))
+def add_task_to_stream(id):
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        priority_level = request.form['priority']
+        stream_id = id
+
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO tasks (title, description, priority_level, stream_id) VALUES (?, ?, ?, ?)',
+                (title, description, priority_level, stream_id)
+            )
+            db.commit()
+
+        tasks_in_stream = db.execute('SELECT * FROM tasks t WHERE stream_id = ?', (id,)).fetchall()
+
+        html_string = '''
+          <button hx-trigger="click" hx-target=".task-list" hx-get="/render_add_task_to_stream_view/{}" hx-swap="innerHTML">Add Task To Stream</button>
+        '''.format(id)
+
+        for row in tasks_in_stream:
+            task = dict(row)
+            html_block = '''
+            <li class="task-card task_wrapper-{}">
+              <p hx-trigger="click" hx-get="/load_task_details/{}" hx-target=".drawer_content" hx-swap="innerHTML" onclick="openDrawer()">
+                {}
+              </p>
+              <div class="task-buttons">
+                <div class="completed_wrapper-{}">
+                <button hx-post="/mark_as_completed/{}" hx-trigger="click" hx-target=".completed_wrapper-{}" hx-swap="outerHTML">
+                  Mark as complete
+                </button>
+                </div>
+                <button hx-post="/delete_task/{}" hx-trigger="click" hx-confirm="Are you sure?" hx-target=".task_wrapper-{}" hx-swap="outerHTML">Delete</button>
+              </div>
+            </li>'''.format(task['id'], task['id'], task['title'], task['id'], task['id'], task['id'], task['id'], task['id'])
+            html_string += html_block
+
+    return html_string
--- flaskr/task.py
+++ flaskr/task.py
@@ -1,24 +1,95 @@
 from flask import (
-    Blueprint, flash, render_template, request, Markup
+    Blueprint, flash, render_template, request, Markup, redirect, url_for
 )
 
 from flaskr.db import get_db
 
 bp = Blueprint('task', __name__)
 
+@bp.route('/load_more_tasks/<page>', methods=(['POST']))
+def load_more_tasks(page):
+   print(page)
+   limit = 5
+   offset = int(page) * limit
+   db = get_db()
+
+   sqliteRows = db.execute(
+       'SELECT t.id, title, description, priority_level, is_completed, created FROM tasks t ORDER BY created DESC LIMIT ? OFFSET ?',
+       (limit, offset,)
+    ).fetchall()
+
+   count = db.execute('SELECT COUNT(*) from tasks').fetchone()[0]
+   tasks = []
+   for row in sqliteRows:
+    tasks.append(dict(row))
+
+   html_string = ''''''
+   
+   for task in tasks:
+      buttons_block = ''
+      is_completed = task['is_completed']
+      class_name = 'completed' if is_completed else 'incomplete'
+      if is_completed:
+        buttons_block += '''
+          <div class="completed_wrapper-{}">
+            <button hx-post="/mark_as_incomplete/{}" hx-trigger="click" hx-target=".task_wrapper-{}" hx-swap="outerHTML">
+              Mark as incomplete
+            </button> 
+          </div>
+        '''.format(task['id'], task['id'], task['id'])
+      else:
+        buttons_block += '''
+          <div class="completed_wrapper-{}">
+            <button hx-post="/mark_as_completed/{}" hx-trigger="click" hx-target=".task_wrapper-{}" hx-swap="outerHTML">
+              Mark as complete
+            </button> 
+          </div>
+        '''.format(task['id'], task['id'], task['id'])
+      html_block = '''
+      <li class="task-card task_wrapper-{} hx-trigger="click" hx-get="/load_task_details/{}" hx-target=".drawer_content" hx-swap="innerHTML">
+        <p hx-trigger="click" hx-target=".task_details_wrapper" hx-swap="innerHTML" hx-post="/load_task_details/{}" class={} onclick="openDrawer()">
+          {}
+        </p>
+        <div class="task-buttons">
+          <div class="completed_wrapper-{}">
+            {}
+          </div>
+          <button hx-post="/delete_task/{}" hx-trigger="click" hx-confirm="Are you sure?" hx-target=".task_wrapper-{}" hx-swap="outerHTML">Delete</button>
+        </div>
+      </li>'''.format(task['id'], task['id'], task['id'], class_name, task['title'], task['id'], buttons_block, task['id'], task['id'])
+      html_string += html_block
+   
+   if count > offset + limit:
+    html_string += '''
+      <div class="paginated-tasks">
+        <button hx-trigger="click" hx-post="/load_more_tasks/{}" hx-target=".paginated-tasks" hx-swap="outerHTML">
+          Load More
+        </button>
+        <p>Showing {} of {}</p>
+      </div>
+      '''.format((int(page) + 1), offset + limit, count)
+   else:
+      html_string += '''
+        <p>Showing {} of {}</p>
+      '''.format(count, count)
+
+   return html_string
+
 @bp.route('/', methods=(['GET']))
 def index():
     db = get_db()
 
     sqliteRows = db.execute(
-       'SELECT t.id, title, description, priority_level, is_completed, created FROM tasks t ORDER BY created DESC'
+       'SELECT t.id, title, description, priority_level, is_completed, created FROM tasks t ORDER BY created DESC LIMIT 5'
     ).fetchall()
 
+    count = db.execute('SELECT COUNT(*) from tasks').fetchone()[0]
     tasks = []
     for row in sqliteRows:
         tasks.append(dict(row))
 
-    return render_template('task/index.html', tasks=tasks)
+    current = len(tasks)
+    return render_template('task/index.html', tasks=tasks, count=count, current=current)
 
 @bp.route('/delete_task/<id>', methods=(['POST']))
 def delete_task(id):
@@ -28,21 +99,51 @@ def delete_task(id):
 
     return ''
 
+# Only handle rendering of the template
+def toggle_task(id, is_completed):
+   db = get_db()
+   updatedRow = db.execute('SELECT * from tasks WHERE id = ?', (id,)).fetchone()
+   db.commit()
+   task = dict(updatedRow)
+
+   button_content = ''
+   class_name = 'completed' if is_completed else 'incomplete'
+
+   if (is_completed):
+      button_content = '''
+        <div class="completed_wrapper-{}">
+          <button hx-post="/mark_as_incomplete/{}" hx-trigger="click" hx-target=".task_wrapper-{}" hx-swap="outerHTML">Mark as incomplete</button>
+        </div>
+      '''.format(id, id, id)
+   else:
+      button_content = '''
+        <div class="completed_wrapper-{}">
+          <button hx-post="/mark_as_completed/{}" hx-trigger="click" hx-target=".task_wrapper-{}" hx-swap="outerHTML">Mark as complete</button>
+        </div>
+      '''.format(id, id, id)
+
+   content = '''
+    <li class="task-card task_wrapper-{}">
+      <p hx-trigger="click" hx-target=".task_details_wrapper" hx-swap="innerHTML" hx-post="/load_task_details/{}" class={} onclick="openDrawer()">
+        {}
+      </p>
+      <div class="task-buttons">
+        {}
+        <button hx-post="/delete_task/{}" hx-trigger="click" hx-confirm="Are you sure?" hx-target=".task_wrapper-{}" hx-swap="outerHTML">Delete</button>
+      </div>
+    </li>
+  '''.format(id, id, class_name, task['title'], button_content, id, id)
+
+   return content
+
 @bp.route('/mark_as_incomplete/<id>', methods=(['POST']))
 def mark_as_incomplete(id):
    db = get_db()
    db.execute('UPDATE tasks SET is_completed = 0 WHERE id = ?', (id,))
    db.commit()
    
-   content = '''
-    <div class="completed_wrapper-{}">
-      <button hx-post="/mark_as_completed/{}" hx-trigger="click" hx-target=".completed_wrapper-{}" hx-swap="innerHTML">
-        Mark as complete
-      </button>
-    </div>
-   '''.format(id, id, id)
+   return toggle_task(id, False)
 
-   return Markup(content)
 
 @bp.route('/mark_as_completed/<id>', methods=(['POST']))
 def mark_as_completed(id):
@@ -50,31 +151,23 @@ def mark_as_completed(id):
    db.execute('UPDATE tasks SET is_completed = 1 WHERE id = ?', (id,))
    db.commit()
 
-   content = '''
-    <div class="completed_wrapper-{}">
-      <button hx-post="/mark_as_incomplete/{}" hx-trigger="click" hx-target=".completed_wrapper-{}" hx-swap="innerHTML">
-        Mark as incomplete
-      </button>
-    </div>
-   '''.format(id, id, id)
-   
-   return Markup(content)
+   return toggle_task(id, True)
 
-@bp.route('/load_task_details/<id>', methods=(['POST']))
-def load_task_details(id):
-  db = get_db()
+# @bp.route('/load_task_details/<id>', methods=(['POST']))
+# def load_task_details(id):
+#   db = get_db()
 
-  sqliteRow = db.execute('SELECT * FROM tasks t WHERE id = ?', (id,)).fetchone()
-  div_content = '''
-    <div>
-      Task Name: {}
-      Task Description: {}
-      Task Priority: {}
-      Is Task Completed: {}
-    </div>
-  '''.format(sqliteRow['title'], sqliteRow['description'], sqliteRow['priority_level'], sqliteRow['is_completed'])
+#   sqliteRow = db.execute('SELECT * FROM tasks t WHERE id = ?', (id,)).fetchone()
+#   div_content = '''
+#     <div>
+#       Task Name: {}
+#       Task Description: {}
+#       Task Priority: {}
+#       Is Task Completed: {}
+#     </div>
+#   '''.format(sqliteRow['title'], sqliteRow['description'], sqliteRow['priority_level'], sqliteRow['is_completed'])
 
-  return Markup(div_content)
+#   return Markup(div_content)
 
 @bp.route('/create', methods=('GET', 'POST'))
 def create():
@@ -99,10 +192,9 @@ def create():
          db.commit()
 
       response_content = '''
-          <div>
-            <p>Task Created Successfully</p>
-            <p>Task name: {}</p>
-            <p>Stream id: {}</p>
+          <div class="success_alert">
+            <h3>Task Created Successfully</h3>
+            <a href="/">Back To Tasks</a>
           </div>
         '''.format(title, stream_id)
 
@@ -115,3 +207,27 @@ def create():
         streams.append(dict(row))
       
     return render_template('task/create.html', streams=streams)
+
+
+@bp.route('/load_task_details/<id>', methods=(['GET']))
+def load_task_details(id):
+  db = get_db()
+  taskRow = db.execute('SELECT * FROM tasks where id = ?', (id,)).fetchone()
+  task = dict(taskRow)
+
+  title = task['title']
+  description = task['description']
+  priority_level = task['priority_level']
+  is_completed = task['is_completed']
+
+  html_content = '''
+    <div class="task_details">
+      <h3>Task Details</h3>
+      <p>Task Name: {}</p>
+      <p>Task Description: {}</p>
+      <p>Task Priority: {}</p>
+      <p>Is Task Completed: {}</p>
+    </div>
+   '''.format(title, description, priority_level, is_completed)
+  
+  return html_content
